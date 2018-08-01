1ab662d0ccaab5a8bdf45832a3ae69446504eabf
Elias J Manzueta <eliasmanz@gmail.com>
2018-07-31 22:06:14 -0400

    Add routing to modules

--- client/app/components/feed/feed.module.js
+++ client/app/components/feed/feed.module.js
@@ -2,5 +2,13 @@ import angular from 'angular';
 import feedComponent from './feed.component';
 
 const feedModule = angular.module('feed', [])
+  .config(($stateProvider) => {
+    "ngInject";
+    $stateProvider
+      .state('feed', {
+        url: '/',
+        component: 'feed'
+      });
+  })
   .component('feed', feedComponent);
 export default feedModule;
